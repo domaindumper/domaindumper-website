9fce5df86834f8c8ba085dc480a277048edea7ca
Shiv Singh <shiv@srapsware.com>
2024-09-08 14:46:09 +0530

    Refactor Header component to update Chakra UI imports and add color mode toggle button

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,35 @@
-// components/Header.js
-import Link from 'next/link';
-
-const Header = () => {
-  return (
-    <header>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
+import {
+    Box,
+    Flex,
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Text,
+    useColorMode,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+  
+  export default function Header() {
+    const { colorMode, toggleColorMode } = useColorMode();
+  
+    return (
+      <Box bg={useColorModeValue('white', 'gray.800')} px={4} py={2}>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Text fontWeight="bold">My Website</Text>
+          <Flex alignItems="center">
+            <Menu>
+              <MenuButton as={IconButton} icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} />
+              <MenuList>
+                <MenuItem onClick={toggleColorMode}>
+                  {colorMode === 'light' ? 'Dark mode' : 'Light mode'}
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+      </Box>
+    );
+  }
